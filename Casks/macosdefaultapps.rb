2cc205021653d8aa26398a928e601cfd49e0c311
klabast <74070528+klabast@users.noreply.github.com>
2026-09-08 13:32:48 +0000

    mda + cask 0.7.0

--- Casks/macosdefaultapps.rb
+++ Casks/macosdefaultapps.rb
@@ -1,6 +1,6 @@
 cask "macosdefaultapps" do
-  version "0.6.1"
-  sha256 "b7218ded39e3f12a906b73602d531df49e8d7e18c4616683c1f5156d25e78dde"
+  version "0.7.0"
+  sha256 "9ebdc248852af8d6e026bc2d8ec2ef399ef945715a00796d8651fe253879a234"
 
   url "https://github.com/klabast/macOSDefaultApps/releases/download/v#{version}/macOSDefaultApps-#{version}.zip"
   name "macOSDefaultApps"
--- Formula/mda.rb
+++ Formula/mda.rb
@@ -1,8 +1,8 @@
 class Mda < Formula
   desc "View and set default application associations on macOS"
   homepage "https://github.com/klabast/macOSDefaultApps"
-  url "https://github.com/klabast/macOSDefaultApps/releases/download/v0.6.1/mda-0.6.1-universal.tar.gz"
-  sha256 "855501ba583491c99779b45c1190788e96a724ba0ca135aac8d51bf6f824b11e"
+  url "https://github.com/klabast/macOSDefaultApps/releases/download/v0.7.0/mda-0.7.0-universal.tar.gz"
+  sha256 "88154afc050c1ee0f6dfef415d07bf0a8a2305f1d482889e10d8b61694ceedc1"
   license "MIT"
 
   depends_on macos: :sequoia
